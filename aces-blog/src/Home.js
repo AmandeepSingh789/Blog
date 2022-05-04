cc30240f58edafab827d094fffa405074386ae30
Amandeep <singh.amandeep5665@gmail.com>
2022-05-03 22:34:13 -0700

    third

--- aces-blog/src/Home.js
+++ aces-blog/src/Home.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Home = () => {
+
+    const[name,setName]=useState('Mario');
+    const [age,setAge]=useState('25');
+    const handleClick = () =>{
+        setName('Luigi');
+        setAge(30);
+    }
+
+
   return (
     <div className='home'>
         <h2>Homepage</h2>
+        <p>{name} is {age} years old</p>
+        <button onClick={handleClick}>Click Me</button>
+
     </div>
   )
 }
 
-export default Home
+export default Home;
