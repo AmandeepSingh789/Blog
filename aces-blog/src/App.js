d659a9eab6c2a45cd73dc9d89ec505432db9ae0a
Amandeep <singh.amandeep5665@gmail.com>
2022-05-03 21:34:27 -0700

    first

---
+++ aces-blog/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'welcome';
+//  const person = { name:"ace", age:30};
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
